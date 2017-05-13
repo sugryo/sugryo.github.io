f171555e3f57530bcfa04f6328be1223bec936c0
Ryo Sugimoto <sugryo1109@gmail.com>
2017-05-13 22:23:49 +0900

    Add hct.js

---
+++ hct/hct.js
@@ -0,0 +1,32 @@
+const URL = '/hst';
+
+new Vue({
+    el: '#app',
+    data: {
+	userinput: '',
+	suggestWords: [],
+	stations: []
+    },
+    methods: {
+	//get_timetables(url, name
+    },
+    computed: {
+	get_weekday_outbound_timetables: function() {
+	    return axios.get(URL + '/weekday_outbound.json');
+	},
+	get_weekday_inbound_timetables: function() {
+	    return axios.get(URL + '/weekday_inbound.json');
+	},
+	get_holiday_outbound_timetables: function() {
+	    return axios.get(URL + '/holiday_outbound.json');
+	},
+	get_holiday_inbound_timetables: function() {
+	    return axios.get(URL + '/holiday_inbound.json');
+	}
+    },
+    watch: {
+	userinput: function(val, oldVal) {
+	    
+	}
+    }
+})
